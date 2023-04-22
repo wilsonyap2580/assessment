18109f3974c4951de2a9824e7cc6963080693259
datouyap <datouyap@gmail.com>
2023-04-22 19:07:10 +0800

    data scrap

--- mudah.py
+++ mudah.py
@@ -19,3 +19,23 @@ link_script_data = json.loads(link_script[0].get_text())
 #for json data
 raw_item_json = []
 
+for data in link_script_data[2]['itemListElement']:
+    item_map = {}
+    #check if dictionary contain item key
+    if 'item' in data:
+        item_url = data['item']['url']
+        item_name = data['item']['name']
+        # check if dictionary contain offers key
+        if 'offers' in data['item']:
+            item_price = data['item']['offers']['price']
+            #convert to float
+            item_price = float(item_price)
+            item_map['price'] = item_price
+        item_map['url'] = item_url
+        item_map['name'] = item_name
+        item_map['website_name'] = website_name
+
+    raw_item_json.append(item_map)
+
+with open('mudah.json', 'w') as file:
+    file.write(json.dumps(raw_item_json))
