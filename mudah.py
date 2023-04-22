d73b79078e7e36d15f2c3a65e32c78c16f574a26
datouyap <datouyap@gmail.com>
2023-04-22 18:41:06 +0800

    Mudah Data

--- mudah.py
+++ mudah.py
@@ -10,6 +10,12 @@ mudah_domain = 'https://www.mudah.my/malaysia/mobile-phones-and-gadgets-for-sale
 
 data = requests.get(mudah_domain)
 html_content = data.text
-soup = BeautifulSoup(html_content, "html_parser")
+soup = BeautifulSoup(html_content, "html.parser")
 
+#data
+link_script = soup.find_all("script", type="application/ld+json")
+link_script_data = json.loads(link_script[0].get_text())
+
+#for json data
 raw_item_json = []
+
