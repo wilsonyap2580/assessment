3bb9af2dc853780d292198296f4e916d1520757c
datouyap <datouyap@gmail.com>
2023-04-23 17:29:48 +0800

    combine both website and display

--- combine.py
+++ combine.py
@@ -17,3 +17,21 @@ with open('compasia.json', 'r') as file:
 
 #sort price in ascending order
 sorted_combined_json = sorted(combined_json, key=lambda k: k['price'], reverse=False)
+
+#save sorted_combined_json into json file named: combined.json
+with open('combined.json','w') as file:
+    file.write(json.dumps(sorted_combined_json))
+
+
+#save in text file
+with open('combined.txt','w') as file:
+    for index, item in enumerate(sorted_combined_json):
+        item_name = item['name']
+        item_price = item['price']
+        item_url = item['url']
+        file.write(f'{index+1}. {item_name} - RM {item_price} - {item_url}\n') 
+
+
+#display in table format with pandas
+df = pd.DataFrame(sorted_combined_json)
+print(df.to_string())  
